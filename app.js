1ab4fab235943fb412bb0412edfff9e12bf4c550
Edson <edsonhmr@gmail.com>
2025-01-30 22:00:09 -0600

    Modificado a prueba

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-// En este desafío, se te pide que desarrolles una función que reciba un array de nombres y seleccione un nombre aleatorio. La función debe devolver el nombre seleccionado. Por ejemplo, si el array de nombres es ["Pedro", "Juan", "Luis", "Ana", "Laura"], la función debe devolver uno de los nombres de la lista, seleccionado aleatoriamente.
-// Para resolver este desafío, deberás seguir los siguientes pasos:
-// 1. Desarrolla una función llamada selectName que reciba un array de nombres como argumento.
-// 2. Dentro de la función, genera un número aleatorio entre 0 y la longitud del array de nombres.
-// 3. Utiliza el número aleatorio para seleccionar un nombre del array.
-// 4. Devuelve el nombre seleccionado.
-// 5. Finalmente, exporta la función selectName.
-// Ejemplo:
-// selectName(["Pedro", "Juan", "Luis", "Ana", "Laura"]);
-// // Devuelve: "Juan"
-// Nota: Puedes utilizar la función Math.random() para generar un número aleatorio.
 
-let array = [];
-let random = Math.floor(Math.random() * array.length);
-let selectName = (array) => {
-    return array[random];
-};
+let amigos = [];
 
-module.exports = selectName;
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        amigos.push(nombre);
+        document.getElementById('amigo').value = '';
+    }
+    document.getElementById('amigo').value = '';  // Limpiar el input
+}
+
+function mostrarAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ''; // Limpiar la lista antes de actualizar
+    for (let amigo of amigos) {
+        let item = document.createElement('li');
+        item.textContent = amigo;
+        lista.appendChild(item);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('No hay amigos para sortear.');
+    } else {
+        let indice = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indice];
+        document.getElementById('resultado').innerHTML = amigoSorteado;
+    }
+}
